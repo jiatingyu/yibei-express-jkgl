8736fc0df951ef83ec750d4e1350e7e13aa98be3
jiatingyu <jiatingyu94@gmail.com>
2023-10-18 15:27:39 +0800

    221

--- index.ts
+++ index.ts
@@ -1,32 +1,34 @@
-import express from "express";
-let cors = require("cors");
-require("express-async-errors");
-import myRouter from "./router/my";
-let host = "127.0.0.1";
-let port = 3000;
-let app = express();
+import express from 'express'
 
-app.use(cors());
-app.use("/my", myRouter);
+// eslint-disable-next-line
+import 'express-async-errors'
+import myRouter from './router/my'
+import cors from 'cors'
+const host = '127.0.0.1'
+const port = 3000
+const app = express()
 
-app.get("/", (req, res) => {
-  console.log("header", req.header("access_token"));
-  res.json({ message: "hello jkgl interface" });
-});
+app.use(cors())
+app.use('/my', myRouter)
+
+app.get('/', (req, res) => {
+  console.log('header', req.header('access_token'))
+  res.json({ message: 'hello jkgl interface' })
+})
 
 app.use((err, req, res, next) => {
-  if (err) {
-    console.error("全局操作捕获1，", err);
-    res.json({ err: err.message });
-    return;
+  if (err != null) {
+    console.error('全局操作捕获1，', err)
+    res.json({ err: err.message })
+    return
   }
-  next();
-});
+  next()
+})
 
-app.use("*", (req, res) => {
-  res.json({ message: "不存在的地址" });
-});
+app.use('*', (req, res) => {
+  res.json({ message: '不存在的地址' })
+})
 
 app.listen(port, () => {
-  console.log(`serve start success, it running http://${host}:${port} `);
-});
+  console.log(`serve start success, it running http://${host}:${port} `)
+})
